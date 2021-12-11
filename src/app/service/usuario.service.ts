1d6c71512a8b2a95da16c95b04113642ab3641de
murallaexe <joksanhaniel@gmail.com>
2021-12-10 23:00:32 -0600

    ultima version

--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -9,16 +9,16 @@ export class UsuarioService {
 
   constructor(private httpClient:HttpClient) { }
   obtenerInformacionUnUsuario(idUsuario:any):Observable<any>{
-    return this.httpClient.get(`http://localhost:8888/usuarios/${idUsuario}`,{});
+    return this.httpClient.get(`http://147.182.185.209:8888/usuarios/${idUsuario}`,{});
   }
   obtenerRegistro(idUsuario:any):Observable<any>{
-    return this.httpClient.get(`http://localhost:8888/usuarios/${idUsuario}/registro`,{});
+    return this.httpClient.get(`http://147.182.185.209:8888/usuarios/${idUsuario}/registro`,{});
   }
   cambiarEstadoCliente(idUsuario:any,idOrden:any,informacion:any):Observable<any>{
-    return this.httpClient.put(`http://localhost:8888/usuarios/${idUsuario}/CambiosEstadoOrdenes/${idOrden}`,{
+    return this.httpClient.put(`http://147.182.185.209:8888/usuarios/${idUsuario}/CambiosEstadoOrdenes/${idOrden}`,{
       estadoOrden:informacion.estado,
       precio:informacion.precio
     })
   }
-  
+
 }
